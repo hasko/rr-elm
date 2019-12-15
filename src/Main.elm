b3807098b0ff51ac2f23f92276801606281ed7c1
hasko <hasko@heinecke.com>
2019-12-15 17:48:18 +0100

    Beginnings of drawing a train

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Attributes as Att
 import Html.Events exposing (onClick)
-import Railroad exposing (Layout)
+import Railroad as RR
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Tuple exposing (first, second)
@@ -15,7 +15,7 @@ main =
 
 
 type alias Model =
-    Railroad.Layout
+    RR.State
 
 
 type Msg
@@ -23,9 +23,9 @@ type Msg
     | Decrement
 
 
-init : Layout
+init : Model
 init =
-    Railroad.sample
+    RR.sample
 
 
 update : Msg -> Model -> Model
@@ -43,12 +43,15 @@ view model =
     div []
         [ svg
             [ height "600", viewBox "0 0 800 600", Att.style "border" "1px solid black" ]
-            (List.map trackToLine model.tracks)
+            [ g [] (List.map trackToSvg model.layout.tracks)
+            , g [] (List.map connectorToSvg (RR.connectors model.layout))
+            , g [] (List.map trainToSvg model.trains)
+            ]
         ]
 
 
-trackToLine : Railroad.Track -> Svg Msg
-trackToLine track =
+trackToSvg : RR.Track -> Svg Msg
+trackToSvg track =
     line
         [ x1 (String.fromInt track.from.position.x)
         , y1 (String.fromInt track.from.position.y)
@@ -56,5 +59,37 @@ trackToLine track =
         , y2 (String.fromInt track.to.position.y)
         , stroke "black"
         , strokeLinecap "round"
+        ]
+        []
+
+
+connectorToSvg : RR.Connector -> Svg Msg
+connectorToSvg conn =
+    circle
+        [ cx (String.fromInt conn.position.x)
+        , cy (String.fromInt conn.position.y)
+        , r "5"
+        , fill "none"
+        , stroke "grey"
+        , strokeWidth "1"
+        ]
+        []
+
+
+trainToSvg : RR.Train -> Svg Msg
+trainToSvg train =
+    g [] (List.map trainSegmentToSvg (RR.tracksForTrain train))
+
+
+trainSegmentToSvg : RR.Track -> Svg msg
+trainSegmentToSvg track =
+    line
+        [ x1 (String.fromInt track.from.position.x)
+        , y1 (String.fromInt track.from.position.y)
+        , x2 (String.fromInt track.to.position.x)
+        , y2 (String.fromInt track.to.position.y)
+        , stroke "red"
+        , strokeLinecap "round"
+        , strokeWidth "5"
         ]
         []
--- src/Railroad.elm
+++ src/Railroad.elm
@@ -1,4 +1,7 @@
-module Railroad exposing (Layout, Track, sample)
+module Railroad exposing (Connector, Layout, State, Track, Train, connectors, sample, tracksForTrain)
+
+import List
+import List.Unique exposing (filterDuplicates)
 
 
 type alias Track =
@@ -17,14 +20,49 @@ type alias Position =
     { x : Int, y : Int }
 
 
-sample : Layout
+type Orientation
+    = Forward
+    | Reverse
+
+
+type alias State =
+    { layout : Layout, trains : List Train }
+
+
+type alias Train =
+    { track : Track, pos : Float, orient : Orientation, length : Int }
+
+
+connectors : Layout -> List Connector
+connectors layout =
+    List.map (\t -> [ t.from, t.to ]) layout.tracks |> List.foldl (++) [] |> filterDuplicates
+
+
+tracksForTrain : Train -> List Track
+tracksForTrain train =
+    --TODO A train can cover multiple tracks.
+    [ train.track ]
+
+
+trackLength : Track -> Float
+trackLength track =
+    sqrt (toFloat ((track.from.position.x - track.to.position.x) ^ 2 + (track.from.position.y - track.to.position.y) ^ 2))
+
+
+sample : State
 sample =
     let
         c1 =
             Connector (Position 100 100)
+
+        t1 =
+            Track c1 (Connector (Position 200 120))
     in
-    { tracks =
-        [ Track c1 (Connector (Position 200 120))
-        , Track c1 (Connector (Position 80 50))
-        ]
+    { layout =
+        { tracks =
+            [ t1
+            , Track c1 (Connector (Position 80 50))
+            ]
+        }
+    , trains = [ Train t1 50 Forward 30 ]
     }
