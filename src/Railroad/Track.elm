c00a14f17bdb62b6ac73077ff201545724dfb7b8
hasko <hasko@heinecke.com>
2021-03-13 16:13:05 +0100

    Json encoder and decoder for Track

--- src/Railroad/Track.elm
+++ src/Railroad/Track.elm
@@ -1,4 +1,7 @@
-module Railroad.Track exposing (Track(..), length)
+module Railroad.Track exposing (Track(..), decoder, encode, length)
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
 
 
 type Track
@@ -19,3 +22,40 @@ length track =
 
         CurvedTrack c ->
             pi * c.radius * c.angle / 180.0
+
+
+encode : Track -> Encode.Value
+encode track =
+    case track of
+        StraightTrack s ->
+            Encode.object
+                [ ( "type", Encode.string "straight" )
+                , ( "length", Encode.float s.length )
+                ]
+
+        CurvedTrack c ->
+            Encode.object
+                [ ( "type", Encode.string "curved" )
+                , ( "radius", Encode.float c.radius )
+                , ( "angle", Encode.float c.angle )
+                ]
+
+
+decoder : Decoder Track
+decoder =
+    Decode.field "type" Decode.string |> Decode.andThen trackDecoder
+
+
+trackDecoder : String -> Decoder Track
+trackDecoder trackType =
+    case trackType of
+        "straight" ->
+            Decode.map (\l -> StraightTrack { length = l }) (Decode.field "length" Decode.float)
+
+        "curved" ->
+            Decode.map2 (\r a -> CurvedTrack { radius = r, angle = a })
+                (Decode.field "radius" Decode.float)
+                (Decode.field "angle" Decode.float)
+
+        other ->
+            Decode.fail ("Trying to decode a track but " ++ other ++ " is not supported.")
