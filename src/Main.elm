7bc23e918a830476e641cd9c29a884a8ff48409f
Heinecke <hasko.heinecke@allianz.de>
2022-04-08 13:37:35 +0800

    Fixed slight logical error

--- src/Main.elm
+++ src/Main.elm
@@ -158,7 +158,7 @@ view model =
                             "Start"
                         )
                     ]
-                , button [ class "btn btn-secondary me-3", onClick Step, disabled (not model.running) ] [ text "Step (1s)" ]
+                , button [ class "btn btn-secondary me-3", onClick Step, disabled model.running ] [ text "Step (1s)" ]
                 , button [ class "btn btn-secondary", onClick Reset ] [ text "Reset" ]
                 ]
             ]
